f60811f89b0d55bd40c54d613fd173b9e4f41f5b
Facundo Petre <fpetre@integraccs.com>
2018-06-10 00:37:36 -0300

    https request on api

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import config from './config'
 let apiKey = config.api_key
 
 export default function getTopArtist(countryName){
-  let URL = `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${ countryName }&api_key=${ apiKey }&format=json`
+  let URL = `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${ countryName }&api_key=${ apiKey }&format=json`
   return fetch(URL)
     .then(res => res.json())
     .then(json => json.topartists.artist)
